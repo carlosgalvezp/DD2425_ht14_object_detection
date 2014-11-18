ea10d301c2217b31213963032bad8b4c10ca1e25
Carlos Galvez <cgdpf@kth.se>
2014-11-18 10:24:54 +0100

    Efficient and robust floor removal. Runs in real-time

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -6,13 +6,16 @@ Object_Detection::Object_Detection(const ros::NodeHandle& n,
                                    const ros::NodeHandle& n_private)
     : n_(n), n_private_(n_private), frame_counter_(0), started_(false), it_(n), detected_object_(false)
 {
+    // ** Load camera extrinsic calibration
+    load_calibration(RAS_Names::CALIBRATION_PATH);
+
     // ** Publishers
     image_pub_ = it_.advertise("/object_detection/image", 1);
     speaker_pub_ = n_.advertise<std_msgs::String>("/espeak/string", 1000);
 
     // ** Subscribers
-    rgb_sub_.subscribe(n_, "/camera/rgb/image_raw", QUEUE_SIZE);
-    depth_sub_.subscribe(n_, "/camera/depth_registered/image_raw",QUEUE_SIZE);
+    rgb_sub_.subscribe(n_, "/camera/rgb/image_rect_color", QUEUE_SIZE);
+    depth_sub_.subscribe(n_, "/camera/depth_registered/hw_registered/image_rect_raw",QUEUE_SIZE);
 
     rgbd_sync_.reset(new RGBD_Sync(RGBD_Sync_Policy(QUEUE_SIZE), rgb_sub_, depth_sub_));
     rgbd_sync_->registerCallback(boost::bind(&Object_Detection::RGBD_Callback, this, _1, _2));
@@ -25,7 +28,7 @@ Object_Detection::Object_Detection(const ros::NodeHandle& n,
     DR_server_.setCallback(DR_f_);
 
     // ** Create ROI
-    ROI_ = cv::Mat::zeros(IMG_ROWS*SCALE_FACTOR, IMG_COLS*SCALE_FACTOR, CV_8UC1);
+    ROI_ = cv::Mat::zeros(IMG_ROWS, IMG_COLS, CV_8UC1);
     for(unsigned int v = 0; v < ROI_.rows; ++v)
     {
         for(unsigned int u = 0; u < ROI_.cols; ++u)
@@ -38,11 +41,12 @@ Object_Detection::Object_Detection(const ros::NodeHandle& n,
 
 }
 
+
 void Object_Detection::RGBD_Callback(const sensor_msgs::ImageConstPtr &rgb_msg,
                                      const sensor_msgs::ImageConstPtr &depth_msg)
 {
     ros::WallTime t_begin = ros::WallTime::now();
-    if(frame_counter_ > 100 || started_)
+    if(frame_counter_ > 20 || started_)
     {
         started_ = true;
         // ** Convert ROS messages to OpenCV images and scale
@@ -51,61 +55,32 @@ void Object_Detection::RGBD_Callback(const sensor_msgs::ImageConstPtr &rgb_msg,
 
         const cv::Mat& rgb_img     = rgb_ptr->image;
         const cv::Mat& depth_img   = depth_ptr->image;
+        cv::Mat rgb_scaled, depth_scaled;
 
-        cv::Mat rgb_img_scaled, depth_img_scaled;
-        cv::resize(rgb_img, rgb_img_scaled, cv::Size(0,0), SCALE_FACTOR, SCALE_FACTOR);
-        cv::resize(depth_img, depth_img_scaled, cv::Size(0,0), SCALE_FACTOR, SCALE_FACTOR);
-        cv::imshow("RGB", rgb_img_scaled);
+        // ** Create point cloud and transform into robot coordinates
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+        buildPointCloud(rgb_img, depth_img, cloud);
+
+        // ** Remove main plane (floor)
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr floor_cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+        get_floor_plane(cloud, floor_cloud);
+
+        // ** Back-project to get binary mask
+        cv::Mat floor_mask = 255*cv::Mat::ones(rgb_img.rows, rgb_img.cols, CV_8UC1);
+        backproject_floor(floor_cloud, floor_mask);
+
+        cv::bitwise_and(floor_mask,ROI_, floor_mask); //Combine with ROI
+        cv::imshow("Floor mask",floor_mask);
         cv::waitKey(1);
 
-        cv::Mat rgb_filtered; //= cv::Mat::zeros(rgb_img.rows, rgb_img.cols, CV_8UC3);
-        rgb_img_scaled.copyTo(rgb_filtered);
+        // ** Apply mask to remove floor in 2D
+        cv::Mat rgb_filtered;
+        rgb_img.copyTo(rgb_filtered, floor_mask);
 
-        hsv_params_.remove_floor = false;
-        if (hsv_params_.remove_floor)
-        {
-            // ** Create point cloud
-            pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-            ros::WallTime t1 = ros::WallTime::now();
-
-            buildPointCloud(rgb_img_scaled, depth_img_scaled, *cloud);
-
-            ros::WallTime t2 = ros::WallTime::now();
-            double t_build_pcl = RAS_Utils::time_diff_ms(t1,t2);
-
-    //        pcl::io::savePCDFile("/home/carlos/test_pcl.pcd", *cloud);
-
-            // ** Remove main plane (floor)
-            t1 = ros::WallTime::now();
-            pcl::PointCloud<pcl::PointXYZRGB>::Ptr floor_cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-
-            get_floor_plane(cloud, floor_cloud);
-
-            t2 = ros::WallTime::now();
-            double t_remove_plane = RAS_Utils::time_diff_ms(t1,t2);
-    //        pcl::io::savePCDFile("/home/carlos/test_pcl.pcd", *floor_cloud);
-
-            // ** Back-project to get binary mask
-            t1 = ros::WallTime::now();
-            cv::Mat floor_mask = 255*cv::Mat::ones(rgb_img_scaled.rows, rgb_img_scaled.cols, CV_8UC1);
-
-            backproject_floor(floor_cloud, floor_mask);
-
-            t2 = ros::WallTime::now();
-            double t_backproject = RAS_Utils::time_diff_ms(t1,t2);
-            cv::bitwise_and(floor_mask,ROI_, floor_mask); //Combine with ROI
-            cv::imshow("Floor mask",floor_mask);
-            cv::waitKey(1);
-
-            // ** Apply mask to remove floor in 2D
-            rgb_img_scaled.copyTo(rgb_filtered, floor_mask);
-
-        }
         cv::imshow("Filtered image",rgb_filtered);
         cv::waitKey(1);
 
         // ** Color analysis (Ryan)
-//        t1 = ros::WallTime::now();
         cv::Point2i mass_center;
         cv::Mat out_image;
         int color = color_analysis(rgb_filtered, mass_center, out_image);
@@ -128,13 +103,13 @@ void Object_Detection::RGBD_Callback(const sensor_msgs::ImageConstPtr &rgb_msg,
                 /// @todo contact brain node in order to switch the navigation mode: move towards robot
             }
 
-            // ** Call recognition if close enough
-//            if(mass_center.y > MIN_MASS_CENTER_Y) // If the object is close enough
-//            {
-                ///@todo contact the brain node so that it can stop the robot
-                ///
+//             ** Call recognition if close enough
+            if(mass_center.y > MIN_MASS_CENTER_Y) // If the object is close enough
+            {
+//                /@todo contact the brain node so that it can stop the robot
+//                /
 
-                // Convert to ROS msg
+//                 Convert to ROS msg
                 cv_bridge::CvImage out_msg;
                 out_msg.header = rgb_msg->header;
                 out_msg.encoding = sensor_msgs::image_encodings::MONO8;
@@ -156,15 +131,11 @@ void Object_Detection::RGBD_Callback(const sensor_msgs::ImageConstPtr &rgb_msg,
                 else{
                     ROS_ERROR("Failed to call recognition service");
                 }
-//            }
-//            else{
-                /// @todo publish mass_center
-//            }
+            }
+            else{
+//                / @todo publish mass_center
+            }
         }
-
-        // ** Print statistics
-//        ROS_INFO("[Object Detection] Build PCL: %.3f,  Plane: %.3f, Backproject: %.3f, Color: %.3f, TOTAL: %.3f ms",
-//                 t_build_pcl, t_remove_plane, t_backproject, t_color, t_total);
     }
     else
         frame_counter_++;
@@ -175,94 +146,75 @@ void Object_Detection::RGBD_Callback(const sensor_msgs::ImageConstPtr &rgb_msg,
 
 void Object_Detection::buildPointCloud(const cv::Mat &rgb_img,
                                        const cv::Mat &depth_img,
-                                             pcl::PointCloud<pcl::PointXYZRGB> &cloud)
-{
-    int height = rgb_img.rows;
-    int width  = rgb_img.cols;
+                                             pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud_out)
+{    
+    int height = rgb_img.rows * SCALE_FACTOR;
+    int width  = rgb_img.cols * SCALE_FACTOR;
+    int step = 1.0 / SCALE_FACTOR;
 
     // ** Construct point cloud
-    cloud.resize(height*width);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    cloud->resize(height*width);
 
-    for (unsigned int u = 0; u < width; ++u)
+    int i = 0;
+    for (unsigned int v = 0; v < rgb_img.rows; v+=step)
     {
-        for (unsigned int v = 0; v < height; ++v)
+        for (unsigned int u = 0; u < rgb_img.cols; u+=step)
         {
             float z_m = depth_img.at<float>(v, u);
 
             const cv::Vec3b& c = rgb_img.at<cv::Vec3b>(v, u);
 
-            pcl::PointXYZRGB& pt = cloud.points[v*width + u];
+            pcl::PointXYZRGB& pt = cloud->points[i++];
 
-            if (z_m != 0)
-            {
-                pt.x = z_m * ((u - CX) * FX_INV);
-                pt.y = z_m * ((v - CY) * FY_INV);
-                pt.z = z_m;
+            pt.x = z_m * ((u - CX) * FX_INV);
+            pt.y = z_m * ((v - CY) * FY_INV);
+            pt.z = z_m;
 
-                pt.r = c[0];
-                pt.g = c[1];
-                pt.b = c[2];
-            }
-            else
-            {
-                pt.x = pt.y = pt.z = 0;//std::numeric_limits<float>::quiet_NaN();
-            }
+            pt.r = c[0];
+            pt.g = c[1];
+            pt.b = c[2];
         }
     }
-    cloud.width = width;
-    cloud.height = height;
-    cloud.is_dense = true;
+    cloud->width = width;
+    cloud->height = height;
+    cloud->is_dense = true;
+
+    // ** Transform into robot coordinate frame
+    pcl::transformPointCloud(*cloud, *cloud_out, t_cam_to_robot_);
 }
 
 void Object_Detection::get_floor_plane(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud_in,
-                                                pcl::PointCloud<pcl::PointXYZRGB>::Ptr      &cloud_out)
+                                             pcl::PointCloud<pcl::PointXYZRGB>::Ptr      &cloud_out)
 {
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
-
-    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-    // Optional
-    seg.setOptimizeCoefficients (true);
-    // Mandatory
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setMaxIterations (1000);
-    seg.setDistanceThreshold (0.005);
-
-    // Segment the largest planar component from the remaining cloud
-    seg.setInputCloud (cloud_in);
-    seg.segment (*inliers, *coefficients);
-    if (inliers->indices.size () == 0)
-    {
-        std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
-    }
-
     // Create the filtering object
-    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-
-    // Extract the object
-    extract.setInputCloud (cloud_in);
-    extract.setIndices (inliers);
-    extract.setNegative (false); //Extract the plane!
-    extract.filter (*cloud_out);
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pass.setInputCloud (cloud_in);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (-1.0, 0.015);
+    pass.filter (*cloud_out);
 }
 
 void Object_Detection::backproject_floor(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &floor_cloud,
                                                                                     cv::Mat &floor_mask)
 {
-    for(std::size_t i = 0; i < floor_cloud->size(); ++i)
+    // ** Transform into camera coordinate frame
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr floor_cloud2(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::transformPointCloud(*floor_cloud, *floor_cloud2, t_robot_to_cam_);
+
+    for(std::size_t i = 0; i < floor_cloud2->size(); ++i)
     {
-        const pcl::PointXYZRGB& pt = floor_cloud->points[i];
+        const pcl::PointXYZRGB& pt = floor_cloud2->points[i];
         if(pt.z != 0)
         {
-            int u = pt.x * FX / (pt.z) + CX;
-            int v = pt.y * FY / (pt.z) + CY;
+            int u = (pt.x * FX / (pt.z) + CX);
+            int v = (pt.y * FY / (pt.z) + CY);
             floor_mask.at<uint8_t>(v,u) = 0;
         }
     }
 
     // ** Opening (dilate + erosion)
-    int size = 1;
+    int size = 2; // This depends on scaling factor
     cv::Mat element = getStructuringElement(cv::MORPH_RECT,cv::Size(2*size+1, 2*size+1),cv::Point( size, size) );
     cv::erode(floor_mask, floor_mask, element);
     cv::dilate(floor_mask, floor_mask, element);
@@ -410,7 +362,6 @@ int Object_Detection::color_analysis(const cv::Mat &img,
 //            }
 //        }
     }
-    cv::resize(out_img, out_img, cv::Size(0,0), 1.0/SCALE_FACTOR, 1.0/SCALE_FACTOR); // So that we can get more detail
     return -1;
 }
 
@@ -458,6 +409,23 @@ void Object_Detection::DR_Callback(ColorTuningConfig &config, uint32_t level)
     hsv_params_.P_display = config.P_Display;
 
     hsv_params_.remove_floor = config.Remove_floor;
+}
+
+void Object_Detection::load_calibration(const std::string &path)
+{
+    std::ifstream file;
+    file.open(path.c_str());
+    t_cam_to_robot_ = Eigen::Matrix4f::Identity();
+    for(unsigned int i = 0; i < 4; ++i)
+    {
+        for(unsigned int j = 0; j < 4; ++j)
+        {
+            file >> t_cam_to_robot_(i,j);
+        }
+    }
+    t_robot_to_cam_ = t_cam_to_robot_.inverse();
+    std::cout << "Read calibration: "<<std::endl << t_cam_to_robot_<<std::endl;
+    file.close();
 }
 
 }  // namespace
